a361a00f66636609050956acd19a1a0b7458f0f9
kawakami <kawakami.ozone@gmail.com>
2021-03-21 19:21:16 +0900

    .

--- tui-rs/src/main.rs
+++ tui-rs/src/main.rs
@@ -6,19 +6,35 @@ use tui::widgets::{Widget, Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
 
 fn main() -> Result<(), io::Error> {
-    //println!("Hello, world!");
-    //
+
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
 
     terminal.draw(|f| {
-        let size = f.size();
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10)
+                ].as_ref()
+            ).split(f.size());
+
         let block = Block::default()
             .title("Block")
             .borders(Borders::ALL);
-        f.render_widget(block, size);
+
+        f.render_widget(block, chunks[0]);
+
+        let block = Block::default()
+            .title("Block 2")
+            .borders(Borders::ALL);
+
+        f.render_widget(block, chunks[1]);
     })
 
 }
