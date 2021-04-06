5ec3a316c350a2a6ceef50b8a5135ffa3c90bfbc
kawakami <kawakami.ozone@gmail.com>
2021-04-06 21:04:03 +0900

    Update a sample in tui-rs.

--- tui-rs/src/main.rs
+++ tui-rs/src/main.rs
@@ -1,40 +1,57 @@
 use std::io;
+use std::io::{stdin, stdout, Read, Write};
 use termion::raw::IntoRawMode;
 use tui::Terminal;
 use tui::backend::TermionBackend;
 use tui::widgets::{Widget, Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
 
-fn main() -> Result<(), io::Error> {
+fn main() {
 
-    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = io::stdout().into_raw_mode().unwrap();
     let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let mut terminal = Terminal::new(backend).unwrap();
 
+    terminal.clear().unwrap();
 
-    terminal.draw(|f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10)
-                ].as_ref()
-            ).split(f.size());
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+    let mut bytes = stdin.bytes();
 
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
+    loop {
 
-        f.render_widget(block, chunks[0]);
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                //.margin(1)
+                .constraints(
+                    [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                    ].as_ref()
+                ).split(f.size());
 
-        let block = Block::default()
-            .title("Block 2")
-            .borders(Borders::ALL);
+            let block = Block::default()
+                .title("Block")
+                .borders(Borders::ALL);
 
-        f.render_widget(block, chunks[1]);
-    })
+            f.render_widget(block, chunks[0]);
+
+            let block = Block::default()
+                .title("Block 2")
+                .borders(Borders::ALL);
+
+            f.render_widget(block, chunks[1]);
+        }).unwrap();
+
+        let b = bytes.next().unwrap().unwrap();
+        match b {
+            b'q' => {
+                break;
+            }
+            _ => { }
+        }
+    }
 
 }
